0c28d6ed7cf3bce23440a454b5f82eacb1811c30
27229-Chayapol <27229@bbw.ac.th>
2026-07-09 14:25:55 +0700

    ใช้ for loop ในการสร้างตารางสูตรคูณ

--- gun.py
+++ gun.py
@@ -1,19 +1,11 @@
-
-print("โปรแกรมตารางสูตรคูณ\n")
-
-
-input_start = int(input("แม่สูตรคูณเริ่มต้น: "))
-input_end = int(input("แม่สูตรคูณสุดท้าย: "))
-
-
-start = min(input_start, input_end)
-end = max(input_start, input_end)
-
-for i in range(start, end + 1):
-    print(f"\nตารางสูตรคูณแม่ {i}")
-    for j in range(1, 13):
-        print(f"{i} x {j} = {i * j}")
-
-print("\n------------------------------")
-print("24 นาย ชยพล เดชทะสร 4/4")
-print("------------------------------")
+print("x", end="\t")
+for col in range(1, 13):
+    print(col, end="\t")
+print()
+ 
+for row in range(1, 13):
+    print(row, end="\t")
+    for col in range(1, 13):   
+        result = row * col        
+        print(result, end="\t")  
+    print()                       
